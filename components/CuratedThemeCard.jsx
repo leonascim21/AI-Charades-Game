2f993b5af59aa7babd0b8f8fe3f74e88ef92848d
Leo <leonascim21@gmail.com>
2024-06-28 21:24:01 -0400

    Finished main layout of curated themes page

--- components/CuratedThemeCard.jsx
+++ components/CuratedThemeCard.jsx
@@ -1,22 +1,31 @@
 import React from "react";
-import { Image, Button } from "react-native";
+import { Image, Text, TouchableOpacity, View, StyleSheet } from "react-native";
 
 const CuratedThemeCard = ({ wordList, navigation, title, picture }) => {
   return (
-    <>
-      <Image
-        source={{
-          width: 100,
-          height: 100,
-          uri: picture,
-        }}
-      ></Image>
-      <Button
-        title={title}
-        onPress={() => navigation.navigate("Game", { wordList })}
-      />
-    </>
+    <TouchableOpacity onPress={() => navigation.navigate("Game", { wordList })}>
+      <Text style={styles.title}>{title}</Text>
+    <Image
+      source={picture}
+      style={styles.image}
+    />
+  </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  title: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 16,
+    textAlign: 'center',
+    padding: 5,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+  }
+});
 
 export default CuratedThemeCard;
--- screens/CuratedThemesScreen.jsx
+++ screens/CuratedThemesScreen.jsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 import {wordList1, wordList2, wordList3} from "../CuratedWordLists";
 import CuratedThemeCard from "../components/CuratedThemeCard";
 import { useEffect } from "react";
@@ -12,12 +12,33 @@ const CuratedThemesScreen = ({ navigation }) => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <CuratedThemeCard wordList={wordList1} navigation={navigation}
-        title="Pirates" picture="https://picsum.photos/100"/>
-      <CuratedThemeCard wordList={wordList2} navigation={navigation}
-        title="Warriors" picture="https://picsum.photos/100"/>
-      <CuratedThemeCard wordList={wordList3} navigation={navigation}
-        title="Pacers" picture="https://picsum.photos/100"/>
+      <View style={styles.row}>
+        <CuratedThemeCard wordList={wordList1} navigation={navigation}
+          title="Animals" picture={require('../assets/CuratedThemeIcons/animals.webp')} />
+        <CuratedThemeCard wordList={wordList2} navigation={navigation}
+          title="Emotions" picture={require('../assets/CuratedThemeIcons/emotions.webp')}/>
+      </View>
+      <View style={styles.row}>
+        <CuratedThemeCard wordList={wordList3} navigation={navigation}
+          title="Accents" picture={require('../assets/CuratedThemeIcons/accents.webp')}/>
+        <CuratedThemeCard wordList={wordList3} navigation={navigation}
+          title="Sports" picture={require('../assets/CuratedThemeIcons/sports.webp')}/>
+      </View>
+
+      <View style={styles.row}>
+        <CuratedThemeCard wordList={wordList3} navigation={navigation}
+          title="Christmas" picture={require('../assets/CuratedThemeIcons/christmas.webp')}/>
+        <CuratedThemeCard wordList={wordList3} navigation={navigation}
+          title="Halloween" picture={require('../assets/CuratedThemeIcons/halloween.webp')}/>
+      </View>
+
+      <View style={styles.row}>
+        <CuratedThemeCard wordList={wordList3} navigation={navigation}
+          title="Act It Out" picture={require('../assets/CuratedThemeIcons/act_it_out.webp')}/>
+        <CuratedThemeCard wordList={wordList3} navigation={navigation}
+          title="Professions" picture={require('../assets/CuratedThemeIcons/professions.webp')}/>
+      </View>
+
     </SafeAreaView>
   );
 };
@@ -27,7 +48,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginVertical: 15,
+    width: '90%'
   },
 });
 
